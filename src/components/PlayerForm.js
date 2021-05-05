b0a6e5d6c9d8d5506a505a35cec6d9e89d9d19ef
Sean <jb@JBs-MacBook-Pro.local>
2021-05-04 21:14:43 -0500

    prop drilled all necessary props

---
+++ src/components/PlayerForm.js
@@ -0,0 +1,12 @@
+// import { Typography } from '@material-ui/core';
+// import React, { useState } from 'react';
+
+// const PlayerForm = () => {
+//   const [player, setPlayer] = useState({});
+
+//   return (
+//     <Typography></Typography>
+//   );
+// }
+
+// export default PlayerForm;
--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import AddPlayer from '../views/AddPlayer';
 import Home from '../views/Home';
 import Roster from '../views/Roster';
 
-function Routes() {
+function Routes({ players, setPlayers }) {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
-      <Route exact path='/add-players/' component={AddPlayer} />
-      <Route exact path='/roster/' component={Roster} />
+      <Route exact path='/add-players/' component={() => <AddPlayer setPlayers={setPlayers} />} />
+      <Route exact path='/roster/' component={() => <Roster players={players} setPlayers={setPlayers} />} />
     </Switch>
   );
 }
+
+Routes.propTypes = {
+  players: PropTypes.array.isRequired,
+  setPlayers: PropTypes.func.isRequired
+};
 
 export default Routes;
