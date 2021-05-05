0df95635e31200624b081912bb9d43e8c613f807
Sean <jb@JBs-MacBook-Pro.local>
2021-05-04 19:31:43 -0500

    added api calls

--- src/helpers/data/rosterData.js
+++ src/helpers/data/rosterData.js
@@ -9,4 +9,32 @@ const getPlayers = () => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-export default getPlayers;
+const createPlayer = (playerObj) => new Promise((resolve, reject) => {
+  axios.patch(`${dbURL}/players.json`, playerObj)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name };
+      axios.patch(`${dbURL}/players/${response.data.name}.json`, body)
+        .then(() => {
+          getPlayers().then(resolve);
+        });
+    })
+    .catch((error) => reject(error));
+});
+
+const deletePlayer = (firebaseKey) => new Promise((reject, resolve) => {
+  axios.delete(`${dbURL}/players/${firebaseKey}.json`)
+    .then(() => {
+      getPlayers().then(resolve);
+    })
+    .catch((error) => reject(error));
+});
+
+const updatePlayer = (player) => new Promise((resolve, reject) => {
+  axios.patch(`${dbURL}/players/${player.firebaseKey}.json`, player)
+    .then(() => getPlayers().then(resolve))
+    .catch((error) => reject(error));
+});
+
+export {
+  getPlayers, createPlayer, deletePlayer, updatePlayer
+};
