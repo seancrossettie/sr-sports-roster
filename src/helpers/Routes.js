6c5ca650744af3d44586ebee58e65905bd8eb42c
Sean <jb@JBs-MacBook-Pro.local>
2021-05-06 19:15:06 -0500

    fixed uid CRUD

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -6,63 +6,30 @@ import Home from '../views/Home';
 import Roster from '../views/Roster';
 import NotFound from '../components/NotFound';
 
-// The PrivateRoute function is creating a private route and returing the specified route based on the props
-// We specify the specific props we want to use in the routeChecker and pass the rest with the spread
 const PrivateRoute = ({ component: Component, user, ...rest }) => {
-  // when we call this function in the return, it is looking for an argument. `props` here is taco.
   const routeChecker = (burrito) => (user
     ? (<Component {...burrito} user={user} />)
     : (<Redirect to={{ pathname: '/', state: { from: burrito.location } }} />));
-    // this render method is one we can use instead of component. Since the components are being dynamically created, we use render. Read the docs for more info: https://reactrouter.com/web/api/Route/render-func
-  // Just like in the routes if we want the dynamically rendered component to have access to the Router props, we have to pass `props` as an argument.
   return <Route {...rest} render={(props) => routeChecker(props)} />;
-};
-
-PrivateRoute.propTypes = {
-  component: PropTypes.func,
-  user: PropTypes.any
 };
 
 function Routes({ user, players, setPlayers }) {
   return (
     <Switch>
-      <Route
-        exact
-        path='/'
-        component={Home}
-      />
-      <Route
-        exact
-        path='/not-found'
-        component={NotFound}
-      />
-      <PrivateRoute
-        exact
-        path='/add-players/'
-        user={user}
-        component={() => <AddPlayer setPlayers={setPlayers} />}
-      />
-      <PrivateRoute
-        exact
-        path='/roster/'
-        user={user}
-        component={() => <Roster players={players} setPlayers={setPlayers} />}
-      />
-      <Route
-        exact
-        path='*'
-        component={NotFound}
-      />
-      <PrivateRoute
-        exact
-        path='*'
-        user={user}
-        component={NotFound}
-      />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/not-found' component={NotFound} />
+      <PrivateRoute exact path='/add-players/' user={user} component={() => <AddPlayer setPlayers={setPlayers} user={user}/>} />
+      <PrivateRoute exact path='/roster/' user={user} component={() => <Roster players={players} setPlayers={setPlayers} user={user} />} />
+      <Route exact path='*' component={NotFound} />
+      <PrivateRoute exact path='*' user={user} component={NotFound} />
     </Switch>
   );
 }
 
+PrivateRoute.propTypes = {
+  component: PropTypes.func,
+  user: PropTypes.any
+};
 Routes.propTypes = {
   players: PropTypes.array.isRequired,
   setPlayers: PropTypes.func.isRequired,
